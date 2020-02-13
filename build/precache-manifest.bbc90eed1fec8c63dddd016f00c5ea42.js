384a61555ed5b067fd074f5644efcc1a41f8860a
heyDante <badalsaibo@gmail.com>
2020-02-13 21:10:11 +0530

    Added build folder from frontend

---
+++ build/precache-manifest.bbc90eed1fec8c63dddd016f00c5ea42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf8404fba2d8339dcf83852fe77d59f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43fe4ae0a41b07dc21d2",
+    "url": "/static/css/main.06bb3341.chunk.css"
+  },
+  {
+    "revision": "dba26719d23bcebd66a4",
+    "url": "/static/js/2.8cb18a68.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8cb18a68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43fe4ae0a41b07dc21d2",
+    "url": "/static/js/main.7fac55d6.chunk.js"
+  },
+  {
+    "revision": "8c3f2c06cc8a63af5092",
+    "url": "/static/js/runtime-main.9f6e6b8d.js"
+  },
+  {
+    "revision": "66db9d677ff9ab9191bd132969247e3f",
+    "url": "/static/media/icons.66db9d67.svg"
+  }
+]);
